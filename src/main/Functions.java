0f652f6ec386ca43425c2d6ba332929024fae2bb
Walker <Walker@LAPTOP-S9DCR6QM>
2023-05-08 15:12:10 -0400

    5/8/23

--- src/main/Functions.java
+++ src/main/Functions.java
@@ -8,7 +8,7 @@ import main.checkingAlgorithms.SymbolChecker;
 public class Functions {
 	CoeffAndVarCalculations calc = new CoeffAndVarCalculations();
 	SymbolChecker symbol = new SymbolChecker();
-	ArrayList<String> calcList = new ArrayList<String>();
+//	ArrayList<String> calcList = new ArrayList<String>();
 	Scanner sc = new Scanner(System.in);
 	public boolean flag = true;
 	
@@ -69,35 +69,16 @@ public class Functions {
 		
 		for (int j=0; j<ls.size(); j++) {
 			if (calc.varExpoList.get(j).contains("^")) {
-//				if (alpha.isAlpha(ls.get(j)) == alpha.alphaVar(evaluation)) {
-					calcList.add(calc.intToString(calc.coeffList.get(j) * (int)Math.pow(calc.stringToInt(evaluation), calc.expoList.get(j))));
-//				}
-//				else {
-//					calcList.add("0");
-//					count++;
-//					System.out.print(ls.get(j) + " ");
-//				}
+				totalSum += (calc.coeffList.get(j) * (int)Math.pow(calc.stringToInt(evaluation), calc.expoList.get(j)));
 			}
 			else if (calc.varExpoList.get(j).contains(alpha.isAlpha(ls.get(j))) && alpha.isAlpha(ls.get(j)) != "") {
-//				if (alpha.isAlpha(ls.get(j)) == alpha.alphaVar(evaluation)) {
-					calcList.add(calc.intToString(calc.coeffList.get(j) * calc.stringToInt(evaluation)));
-//				}
-//				else {
-//					calcList.add("0");
-//					count++;
-//					System.out.print(ls.get(j) + " ");
-//				}
+				totalSum += calc.coeffList.get(j) * calc.stringToInt(evaluation);
 			}
 			else {
-				calcList.add(calc.intToString(calc.coeffList.get(j)));
+				totalSum += calc.coeffList.get(j);
 			}
-			totalSum += calc.stringToInt(calcList.get(j));
 		}
-		if (totalSum < 0 && count != 0)
-			System.out.println("- " + (totalSum * -1));
-		else if (totalSum > 0 && count != 0)
-			System.out.println("+ " + totalSum);
-		else
-			System.out.println(totalSum);
+		System.out.println(totalSum);
+		totalSum = 0;
 	}
 }
---
+++ src/main/notes.java
@@ -0,0 +1,30 @@
+//package main;
+//
+//public class notes {
+//	for (int j=0; j<ls.size(); j++) {
+//		if (calc.varExpoList.get(j).contains("^")) {
+//			if (alpha.isAlpha(ls.get(j)) == alpha.alphaVar(evaluation)) {
+//				calcList.add(calc.intToString(calc.coeffList.get(j) * (int)Math.pow(calc.stringToInt(evaluation), calc.expoList.get(j))));
+//			}
+//			else {
+//				calcList.add("0");
+//				count++;
+//				System.out.print(ls.get(j) + " ");
+//			}
+//		}
+//		else if (calc.varExpoList.get(j).contains(alpha.isAlpha(ls.get(j))) && alpha.isAlpha(ls.get(j)) != "") {
+//			if (alpha.isAlpha(ls.get(j)) == alpha.alphaVar(evaluation)) {
+//				calcList.add(calc.intToString(calc.coeffList.get(j) * calc.stringToInt(evaluation)));
+//			}
+//			else {
+//				calcList.add("0");
+//				count++;
+//				System.out.print(ls.get(j) + " ");
+//			}
+//		}
+//		else {
+//			calcList.add(calc.intToString(calc.coeffList.get(j)));
+//		}
+//		totalSum += calc.stringToInt(calcList.get(j));
+//	}
+//}
